7f9422e07091bdbb80746008f2aea6a55940accb
Josh Pollock <jopo@mit.edu>
2024-07-29 19:55:57 -0400

    render a series of items to the screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import type { Component } from "solid-js";
 import { Plot } from "./plot";
 import { Rect } from "./marks/Rect";
 import { Circle } from "./marks/Circle";
+import { AlphabetType, alphabet } from "./datasets/alphabet";
+import { DiscreteLayout } from "./discrete";
 
 const App: Component = () => {
   return (
@@ -9,6 +11,15 @@ const App: Component = () => {
       <Plot width={500} height={500}>
         <Rect x={100} y={100} width={100} height={100} fill="red" />
         <Circle x={200} y={200} width={100} height={100} fill="blue" />
+        <DiscreteLayout
+          data={alphabet}
+          x={(d: AlphabetType) => d.frequency * 5000}
+          y={(d: AlphabetType) => 30}
+          mark={Rect}
+          width={10}
+          height={10}
+          fill="cornflowerblue"
+        />
       </Plot>
     </div>
   );
--- src/discrete.tsx
+++ src/discrete.tsx
@@ -1,4 +1,22 @@
+import { Component, For, splitProps } from "solid-js";
+import { Dynamic } from "solid-js/web";
+
 // Discrete Layout. For dots and bars and stuff
-export const DiscreteLayout = () => {
-  return <div>Discrete Layout</div>;
+export type DiscreteLayoutProps<T> = {
+  data: T[];
+  x: (d: T) => number;
+  y: (d: T) => number;
+  mark: Component<{ x: number; y: number }>;
+};
+
+export const DiscreteLayout = <T,>(props: DiscreteLayoutProps<T>) => {
+  const [_, passThroughProps] = splitProps(props, ["data", "x", "y", "mark"]);
+
+  return (
+    <g>
+      <For each={props.data}>
+        {(d) => <Dynamic component={props.mark} {...passThroughProps} x={props.x(d)} y={props.y(d)} />}
+      </For>
+    </g>
+  );
 };
